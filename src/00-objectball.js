4848ae87b70a7a02e66efb8889f3b44be7779229
Kennedy Ouma Odero <kennedyrogouma222@gmail.com>
2025-03-10 20:27:13 +0300

    added functions

--- src/00-objectball.js
+++ src/00-objectball.js
@@ -1,3 +1,5 @@
+
+
 function gameObject() {
     return {
         home: {
@@ -124,4 +126,263 @@ function gameObject() {
         }     
     }
 }
-console.log(gameObject())
+
+
+// function to call game objects 
+
+const game = gameObject();
+console.log(game);
+
+
+// function to call home team (using bracket notation)
+
+function homeTeam() {
+    return game['home'] ['teamName'];
+}
+console.log(homeTeam());
+
+// function to call home team (using dot notation)
+function homeTeam() {
+    return game.home.teamName;
+}
+console.log(homeTeam());
+
+
+// function to call home team players (using dot notation)
+function homeTeamPlayers() {
+    return game.home.players;
+}
+console.log(homeTeamPlayers());
+
+// function to call home team players (using bracket notation)
+function homeTeamPlayers() {
+    return game['home'] ['players'];
+}
+console.log(homeTeamPlayers());
+
+
+// function to call away team (using dot notation)
+function awayTeam() {
+    return game.away.teamName;
+}
+console.log(awayTeam());
+// function to call away team (using bracket notation)
+function awayTeam() {
+    return game['away'] ['teamName'];
+}
+console.log(awayTeam());
+
+// function to call away team players (using dot notation)
+function awayTeamPlayers() {
+    return game.away.players;
+}
+console.log(awayTeamPlayers());
+
+// function to call away team players (using bracket notation)
+function awayTeamPlayers() {
+    return game['away'] ['players'];
+}
+console.log(awayTeamPlayers());
+
+
+// function to call team using the team name
+function team(teamName) {
+    return teamName === game.home.teamName ? game.home : game.away;
+}
+console.log(team('Brooklyn Nets'));
+console.log(team('Charlotte Hornets'));
+
+
+
+// function to call team colors using the team function
+function teamColors(teamName) {
+    return team(teamName).colors;
+}
+console.log(teamColors('Brooklyn Nets'));
+console.log(teamColors('Charlotte Hornets'));
+
+
+
+// function to call player stats using the team and playerName
+function getPlayerStats(team, playerName) {
+    return game[team].players[playerName];
+}
+console.log(getPlayerStats('home', 'player1'));
+console.log(getPlayerStats('home', 'player2'));
+console.log(getPlayerStats('home', 'player3'));
+console.log(getPlayerStats('home', 'player4'));
+console.log(getPlayerStats('home', 'player5'));
+
+console.log(getPlayerStats('away', 'player1'));
+console.log(getPlayerStats('away', 'player2'));
+console.log(getPlayerStats('away', 'player3'));
+console.log(getPlayerStats('away', 'player4'));
+console.log(getPlayerStats('away', 'player5'));
+
+
+
+// function to call the winning team using the team points
+function winningTeam() {
+    return game.home.points > game.away.points ? game.home.teamName : game.away.teamName;
+}
+console.log(winningTeam());
+
+
+
+// function to call all team players using the team name
+function getTeamPlayers(teamName) {
+
+    if (teamName === 'Brooklyn Nets' || teamName === 'Charlotte Hornets') {
+        return team(teamName).players;
+    }
+    else
+        return "Insert team input. Please use 'Brooklyn Nets' or 'Charlotte Hornets'.";
+}
+console.log(getTeamPlayers('Brooklyn Nets'));
+console.log(getTeamPlayers('Charlotte Hornets'));
+
+
+
+// function to get the number of points scored by a player
+function getPlayerPoints(teamName, playerName) {
+    return team(teamName).players[playerName].points;
+}
+console.log(getPlayerPoints('Brooklyn Nets', 'player1'));
+console.log(getPlayerPoints('Brooklyn Nets', 'player2'));
+console.log(getPlayerPoints('Brooklyn Nets', 'player3'));
+console.log(getPlayerPoints('Brooklyn Nets', 'player4'));
+console.log(getPlayerPoints('Brooklyn Nets', 'player5'));
+
+console.log(getPlayerPoints('Charlotte Hornets', 'player1'));
+console.log(getPlayerPoints('Charlotte Hornets', 'player2'));
+console.log(getPlayerPoints('Charlotte Hornets', 'player3'));
+console.log(getPlayerPoints('Charlotte Hornets', 'player4'));
+console.log(getPlayerPoints('Charlotte Hornets', 'player5'));
+
+// function that retrieves the total points scored by all players of each team
+
+function teamPoints(teamName) {
+    return Object.values(getTeamPlayers(teamName)).reduce((acc, player) => acc + player.points, 0);
+}
+console.log(teamPoints('Brooklyn Nets'));
+console.log(teamPoints('Charlotte Hornets'));
+
+
+// alternatively B
+
+function getTeamPoints(teamName) {
+    let totalPoints = 0;
+    let teamPlayers;
+    if (teamName === 'Brooklyn Nets' || teamName === 'Charlotte Hornets') {
+        teamPlayers = getTeamPlayers(teamName);
+        for (let player in teamPlayers) {
+            totalPoints += teamPlayers[player].points;
+        }
+        return totalPoints;
+    }
+    else
+        return "Insert team input. Please use 'Brooklyn Nets' or 'Charlotte Hornets'.";
+}
+console.log(getTeamPoints('Brooklyn Nets'));
+console.log(getTeamPoints('Charlotte Hornets'));
+console.log(getTeamPoints('Orlando Pirates'));
+
+
+// alternatively C
+function getTeamPoints(teamName) {
+    let totalPoints = 0;
+    let teamPlayers;
+    if (teamName === 'Brooklyn Nets' || teamName === 'Charlotte Hornets') {
+        teamPlayers = game.home.players;
+        } else if (teamName === 'Charlotte Hornets') {
+            teamPlayers = game.away.players;
+        } else {
+            return "Input a valid team name i.e. 'Brooklyn Nets' or 'Charlotte Hornets'.";
+        }
+        for (let player in teamPlayers) {
+            totalPoints += teamPlayers[player].points;
+        }
+        return totalPoints;
+    }
+    console.log(getTeamPoints('Brooklyn Nets'));
+    console.log(getTeamPoints('Charlotte Hornets'));
+    console.log(getTeamPoints('Manchester United'));
+
+
+// function to call player numbers using the spread operator
+function getPlayerNumbers() {
+    const game = gameObject();
+    const allPlayers = { ...game.home.players, ...game.away.players };
+    const playerNumbers = [];
+    for (let player in allPlayers) {
+        playerNumbers.push(allPlayers[player].number);
+    }
+    return playerNumbers;
+}
+console.log(JSON.stringify(getPlayerNumbers(), null, 2));
+
+
+// function to return big shoe rebounds
+function bigShoeRebounds() {
+    const allPlayers = { ...game.home.players, ...game.away.players };
+    let playerWithBiggestShoe = '';
+    let biggestShoeSize = 0;
+    for (let player in allPlayers) {
+        if (allPlayers[player].shoe > biggestShoeSize) {
+            biggestShoeSize = allPlayers[player].shoe;
+            playerWithBiggestShoe = allPlayers[player];
+        }
+    }
+    return playerWithBiggestShoe.rebounds;
+}
+console.log(bigShoeRebounds());
+
+
+// function to return the player with the longest name
+function longestName() {
+    const allPlayers = { ...game.home.players, ...game.away.players };
+    let playerWithLongestName = '';
+    let longestNameLength = 0;
+    for (let player in allPlayers) {
+        if (allPlayers[player].name.length > longestNameLength) {
+            longestNameLength = allPlayers[player].name.length;
+            playerWithLongestName = allPlayers[player];
+        }
+    }
+    return playerWithLongestName.name;
+}
+console.log(longestName());
+
+// function to return the player with the longest name had the most steals
+function getMostSteals() {
+    const allPlayers = { ...game.home.players, ...game.away.players };
+    let playerWithMostSteals = '';
+    let mostSteals = 0;
+    for (let player in allPlayers) {
+        if (allPlayers[player].steals > mostSteals) {
+            mostSteals = allPlayers[player].steals;
+            playerWithMostSteals = allPlayers[player];
+        }
+    }
+    return playerWithMostSteals.name;
+}
+console.log(getMostSteals());
+
+// function that returns true if the player with the longest name had the most steals
+function playerWithLongestNameHadMostSteals() {
+    return longestName() === getMostSteals();
+}
+console.log(playerWithLongestNameHadMostSteals());
+
+
+
+
+// // function to call all players using spread operator ( we used the spread operator ... to merge the two objects)
+// function getAllPlayers() {
+//     const game = gameObject();
+//     const allPlayers = { ...game.home.players, ...game.away.players };
+//     return allPlayers;
+// }
+// console.log(getAllPlayers());
+
+
